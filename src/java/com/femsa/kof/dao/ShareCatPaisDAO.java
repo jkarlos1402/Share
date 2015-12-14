fceed72cf3905035719a3ae83915f6b20f533a71
TMXIDSJPINAM <TMXIDSJPINAM@JCARLOS.na.kof.ccf>
2015-12-14 17:58:55 -0600

    inicio validacion de catalogos

--- src/java/com/femsa/kof/dao/ShareCatPaisDAO.java
+++ src/java/com/femsa/kof/dao/ShareCatPaisDAO.java
@@ -8,14 +8,34 @@ import javax.persistence.Query;
 
 public class ShareCatPaisDAO {
 
+    private String error = "";
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
     public List<ShareCatPais> getCatPais() {
         JPAUtil jpau = new JPAUtil();
         EntityManager em = jpau.getEntityManager();
         Query query = em.createQuery("SELECT pais FROM ShareCatPais pais WHERE pais.idstatus = 1");
         List<ShareCatPais> countries = query.getResultList();
         em.clear();
-        em.close();       
+        em.close();
         return countries;
+    }
+
+    public ShareCatPais getCatPais(String nombrePais) {
+        JPAUtil jpau = new JPAUtil();
+        EntityManager em = jpau.getEntityManager();
+        Query query = em.createQuery("SELECT pais FROM ShareCatPais pais WHERE pais.nombre = " + nombrePais.toUpperCase() + " AND pais.idstatus = 1");
+        ShareCatPais country = (ShareCatPais) query.getSingleResult();
+        em.clear();
+        em.close();
+        return country;
     }
 
     public List<ShareCatPais> getCatPaisAll() {
@@ -35,18 +55,24 @@ public class ShareCatPaisDAO {
         Query query = null;
         boolean flagOk = true;
         try {
-            et.begin();            
-            if (pais.getPkPais() != null) {
-                em.merge(pais);
-            } else {
-                query = em.createNativeQuery("CREATE TABLE " + pais.getNombreTabla() + " (PAIS VARCHAR2(50 BYTE),CANAL VARCHAR2(50 BYTE), "
-                        + "FECHA VARCHAR2(50 BYTE), GRUPO_CATEGORIA VARCHAR2(50 BYTE), CATEGORIA VARCHAR2(50 BYTE), "
-                        + "FABRICANTE VARCHAR2(100 BYTE), VOLUMEN_MES NUMBER, VENTA_MES NUMBER)");
-                query.executeUpdate();
-                em.persist(pais);
+            et.begin();
+            if (getCatPais(pais.getNombre()) == null) {
+                if (pais.getPkPais() != null) {
+                    em.merge(pais);
+                } else {
+                    query = em.createNativeQuery("CREATE TABLE " + pais.getNombreTabla() + " (PAIS VARCHAR2(50 BYTE),CANAL VARCHAR2(50 BYTE), "
+                            + "FECHA VARCHAR2(50 BYTE), GRUPO_CATEGORIA VARCHAR2(50 BYTE), CATEGORIA VARCHAR2(50 BYTE), "
+                            + "FABRICANTE VARCHAR2(100 BYTE), VOLUMEN_MES NUMBER, VENTA_MES NUMBER)");
+                    query.executeUpdate();
+                    em.persist(pais);
+                }
+            }else{
+                error = "Country already exists";
+                flagOk = false;
             }
             et.commit();
-        } catch (Exception  e) {            
+        } catch (Exception e) {
+            error = e.getMessage();
             if (et.isActive()) {
                 et.rollback();
             }
